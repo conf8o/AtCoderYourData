f28ff5f59100c530d08901982338bfed85892daa
conf8o <8confeito8@gmail.com>
2020-06-21 03:25:34 +0900

    make view contexts compact

--- Sources/App/Models/ViewContexts/BodyContents.swift
+++ Sources/App/Models/ViewContexts/BodyContents.swift
@@ -2,3 +2,20 @@ struct BodyContents: Encodable {
     var basicStatsList: [BasicStats]
     var concreteDataList: [ConcreteData]
 }
+
+struct BasicStats: Encodable {
+    var title: String
+    var mean: Double
+    var std: Double
+    var min: Double
+    var lowMid: Double
+    var mid: Double
+    var highMid: Double
+    var max: Double
+}
+
+struct ConcreteData: Encodable {
+    var type: String
+    var columnsBase: String
+    var rowsBase: String
+}
--- Sources/App/Models/ViewContexts/ConcreteData.swift
+++
@@ -1,7 +0,0 @@
-import Vapor
-
-struct ConcreteData: Encodable {
-    var type: String
-    var columnsBase: String
-    var rowsBase: String
-}
